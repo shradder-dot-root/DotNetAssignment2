c087597de14f2833871d1d7c480f9a5c8da38c30
Kamalesh Jikamade <64322686+shradder-dot-root@users.noreply.github.com>
2022-11-28 08:37:28 +0530

    Extension Method Exercise

---
+++ ExtensionMethodExercise/ExtensionMethodExercise/ActionClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodExercise
+{
+    public class ActionClass
+    {
+
+        public string GetString { get; set; }
+        Console.WriteLine("Enter string");
+                string j = Console.ReadLine();
+        Console.WriteLine(j);
+    }
+}
---
+++ ExtensionMethodExercise/ExtensionMethodExercise/MakeUpperCaseExtensionMethod.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodExercise
+{
+    public static class MakeUpperCaseExtensionMethod
+    {
+        public static string ChangeToUpperCase(this string userString)
+        {
+        if (userString.Length > 0)
+            {
+                string outputString = userString.ToUpper();
+                //Console.WriteLine(outputString);
+                return outputString;
+            }
+            return userString;
+        }
+    }
+}
