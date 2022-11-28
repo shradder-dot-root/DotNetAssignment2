95c19e27b60a91ebf091c3b158aa74371104962e
Kamalesh Jikamade <64322686+shradder-dot-root@users.noreply.github.com>
2022-11-28 08:43:55 +0530

    Action Delegate Assignment

---
+++ ActionDelegateExercise/ActionDelegateExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ActionDelegateExercise.SquareRoot;
+
+namespace ActionDelegateExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Action delegate with Anonymous function  
+
+            Action<string> printString = delegate (string Uinput)
+            {
+                Console.WriteLine("Inside anonymous Function");
+                Console.WriteLine(Uinput);
+            };
+            printString("Hello its a Action Delegate..");
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
