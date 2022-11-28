b27d5e8f74ad20907f156069413b5dfd6219c031
Kamalesh Jikamade <64322686+shradder-dot-root@users.noreply.github.com>
2022-11-28 08:42:11 +0530

    Linq Queries on Employee Data

---
+++ DotNetTraining/DotNetTraining/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetTraining
+{
+    class Employee
+    {
+        private int id;
+        private int salary;
+        private string name;
+        public Employee(int id,string name,int salary)
+        {
+            this.id = id;
+            this.name = name;
+            this.salary = salary;
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+        
+
+    }
+
+
+    
+
+
+    
+}
---
+++ DotNetTraining/DotNetTraining/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DotNetTraining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating List for storing multiple employees at single place
+            List < Employee > empList = new List<Employee>();
+            empList.Add(new Employee(001, "Kamalesh Jikamade",5058900));
+            empList.Add(new Employee(002, "Siddhesh Jikamade", 986700));
+            empList.Add(new Employee(003, "Abhishek Jikamade", 8964100));
+            empList.Add(new Employee(004, "Viraj Jikamade", 851100));
+            empList.Add(new Employee(005, "Bhavana Jikamade", 985100));
+            empList.Add(new Employee(006, "Ashwini Jikamade", 8964100));
+
+
+            //printing the employee list 
+            foreach (var item in empList)
+            {
+                Console.WriteLine("Employee Datails As Follows ");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Employee Details :{0},{1},{2}",item.Id,item.Name,item.Salary);
+            }
+
+
+            //Linq query to sort employee salary in descending order
+            List<int> orderBySalary2 = new List<int>();
+            var orderBySalary = from sal in empList
+                                orderby sal.Salary descending
+                                select sal;
+            
+            Console.WriteLine("\n------------Descending Salary ---------------------");
+            foreach (var descSalary in orderBySalary)
+            {
+                
+                Console.WriteLine(descSalary.Salary);
+                
+            }
+
+            //Linq query to fetch all employees  whose name starts with particular 
+            var querySyntax1 = from employee in empList
+                               where employee.Name.StartsWith("S")
+                               select employee.Name;
+
+
+
+            Console.WriteLine("------------Available String in Data----------------");
+            foreach (var item in querySyntax1)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            //Linq query to delete all employees  whose name starts with particular 
+            var authors = empList.Where(a => a.Name.StartsWith("K"));
+            empList = empList.Except(authors).ToList();
+            empList = empList.Except(empList.Where(a => a.Name.StartsWith ("S"))).ToList();
+
+            foreach (var item in empList)
+            {
+                Console.WriteLine("Emplloyee Reamains After Removing :");
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Employee Details :{0},{1},{2}", item.Id, item.Name, item.Salary);
+            }
+
+        }
+    }
+}
